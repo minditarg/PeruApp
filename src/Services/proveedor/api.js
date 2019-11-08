769f4d553a897c23bc0cfeec88afb5ba2d5eb4ff
ramiroferrari <ramiroferrari92@gmail.com>
2019-11-08 17:17:57 -0300

    subir img

--- src/Services/proveedor/api.js
+++ src/Services/proveedor/api.js
@@ -10,9 +10,10 @@ const endPoints = {
 export const listado = () => fetchApi(endPoints.listado, {}, 'post');
 
 
-export const crear = (nombre, email, descripcion, direccion, telefono, foto, usuarioId) =>
-    fetchApi(endPoints.crear, { nombre: nombre, email: email, descripcion: descripcion, direccion: direccion, telefono: telefono, foto: foto, usuarioId: usuarioId }, 'post');
-
+export const crear = (nombre, email, descripcion, direccion, telefono, foto, usuarioId) => {
+    return fetchApi(endPoints.crear, { nombre: nombre, email: email, descripcion: descripcion, direccion: direccion, telefono: telefono, foto: foto, usuarioId: usuarioId }, 'post', true);
+}
+    
 
 export const actualizar = (id) =>
     fetchApi(endPoints.actualizar + id, { nombre: nombre, email: email, descripcion: descripcion, direccion: direccion, telefono: telefono, foto: foto }, 'put');
