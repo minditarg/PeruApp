6858e40ded0fc2a10fbd04fcbee186ab7d68d198
ramiroferrari <ramiroferrari92@gmail.com>
2019-11-20 10:42:59 -0300

    config

--- src/Services/api/config.js
+++ src/Services/api/config.js
@@ -2,6 +2,6 @@ import { API_URL } from 'react-native-dotenv'
 export default {
 	// url: 'http://50.63.166.215:5001/api',
 	// pathFiles: 'http://50.63.166.215:5001/uploads/',
-	 url:  'http://192.168.0.100:3001/api',
-	 pathFiles: 'http://192.168.0.100:3001/uploads/',
+	 url:  'http://10.30.30.125:3001/api',
+	 pathFiles: 'http://10.30.30.125:3001/uploads/',
 };
